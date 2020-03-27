8c340371eb510b6ae00af55757e18c73e6554d56
Vladislav Yarmak <vladislav@vm-0.com>
2020-03-27 22:20:36 +0200

    make SafeWrite actually safe
    consistency fix
    
    SafeWrite: use only one fd
    
    file: add tests
    
    SafeWrite: revert function declaration
    
    linting fixes
    
    fix linting for tests as well

--- file/file.go
+++ file/file.go
@@ -11,16 +11,39 @@ import (
 // SafeWrite writes data to a temporary file and then renames it to what's specified in path
 func SafeWrite(path string, data []byte) error {
 	dir := filepath.Dir(path)
+
 	err := os.MkdirAll(dir, 0755)
 	if err != nil {
 		return err
 	}
 
-	tmpPath := path + ".tmp"
-	err = ioutil.WriteFile(tmpPath, data, 0644)
+	// Ensure multiple simultaneous callers will not choose the same file.
+	tmpFile, err := ioutil.TempFile(dir,  "tmp")
 	if err != nil {
 		return err
 	}
 
-	return os.Rename(tmpPath, path)
+	tmpPath := tmpFile.Name()
+	// Don't leak temp files left by failed write attempts
+	defer func() {
+		tmpFile.Close()
+		if err != nil {
+			_ = os.Remove(tmpPath)
+		}
+	}()
+
+	_, err = tmpFile.Write(data)
+	if err != nil {
+		return err
+	}
+
+	// Change file mode to retain compat with old version of function
+	err = tmpFile.Chmod(0644)
+	if err != nil {
+		return err
+	}
+
+	// Assign err explicitly to make defer func aware about error
+	err = os.Rename(tmpPath, path)
+	return err
 }
