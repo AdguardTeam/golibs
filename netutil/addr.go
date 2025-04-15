234bcba92230adbefb8abbaa44cf84358fb09543
Dimitry Kolyshev <dkolyshev@adguard.com>
2025-04-15 11:41:21 +0700

    netutil: added IsValidMACString with tests

--- netutil/addr.go
+++ netutil/addr.go
@@ -157,6 +157,95 @@ func ValidateMAC(mac net.HardwareAddr) (err error) {
 	}
 }
 
+// IsValidMACString is a best-effort check to determine if s is a valid MAC
+// address before using [net.ParseMAC], aimed at reducing allocations.
+func IsValidMACString(s string) (ok bool) {
+	if len(s) < 14 {
+		return false
+	}
+
+	if s[2] == ':' || s[2] == '-' {
+		return hasValidMACFragments(s)
+	} else if s[4] == '.' {
+		return hasValidMACLongFragments(s)
+	}
+
+	return false
+}
+
+// hasValidMACLongFragments returns true if s contains valid MAC address
+// hex digits.
+func hasValidMACFragments(s string) (ok bool) {
+	if (len(s)+1)%3 != 0 {
+		return false
+	}
+
+	n := (len(s) + 1) / 3
+	if n != 6 && n != 8 && n != 20 {
+		return false
+	}
+
+	for x, i := 0, 0; i < n; i++ {
+		if !startsWith2Hex(s[x:], s[2]) {
+			return false
+		}
+
+		x += 3
+	}
+
+	return true
+}
+
+// hasValidMACLongFragments returns true if s contains valid long MAC address
+// hex fragments.
+func hasValidMACLongFragments(s string) (ok bool) {
+	if (len(s)+1)%5 != 0 {
+		return false
+	}
+
+	n := 2 * (len(s) + 1) / 5
+	if n != 6 && n != 8 && n != 20 {
+		return false
+	}
+
+	for x, i := 0, 0; i < n; i += 2 {
+		if !startsWith2Hex(s[x:x+2], 0) {
+			return false
+		}
+
+		if !startsWith2Hex(s[x+2:], s[4]) {
+			return false
+		}
+
+		x += 5
+	}
+
+	return true
+}
+
+// isValidHexChar returns true if c is a valid hexadecimal digit character.
+func isValidHexChar(c byte) (ok bool) {
+	return ('0' <= c && c <= '9') || ('a' <= c && c <= 'f') || ('A' <= c && c <= 'F')
+}
+
+// startsWith2Hex checks if the next two hex digits of s could be converted into
+// a byte.  If s is longer than 2 bytes then the third byte must be e.  If the
+// first two bytes of s are not hex digits or the third byte does not match e,
+// false is returned.
+func startsWith2Hex(s string, e byte) (ok bool) {
+	if s == "" || len(s) > 2 && s[2] != e {
+		return false
+	}
+
+	for i := 0; i < 2; i++ {
+		if !isValidHexChar(s[i]) {
+			return false
+		}
+	}
+
+	return true
+}
+
 // MaxDomainLabelLen is the maximum allowed length of a domain name label
 // according to [RFC 1035].
 //
--- netutil/addr_test.go
+++ netutil/addr_test.go
@@ -91,6 +91,175 @@ func TestValidateMAC(t *testing.T) {
 	}
 }
 
+func TestIsValidMACString(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		want assert.BoolAssertionFunc
+		name string
+		in   string
+	}{{
+		want: assert.True,
+		name: "good_eui_48",
+		in:   "00:01:02:03:04:05",
+	}, {
+		want: assert.True,
+		name: "good_eui_48_dot",
+		in:   "0000.5e00.5301",
+	}, {
+		want: assert.True,
+		name: "good_eui_48_hyphen",
+		in:   "00-00-5e-00-53-01",
+	}, {
+		want: assert.True,
+		name: "good_eui_64",
+		in:   "00:01:02:03:04:05:06:07",
+	}, {
+		want: assert.True,
+		name: "good_infiniband",
+		in:   "00:01:02:03:04:05:06:07:08:09:0a:0b:0c:0d:0e:0f:10:11:12:13",
+	}, {
+		want: assert.False,
+		name: "bad_empty",
+		in:   "",
+	}, {
+		want: assert.False,
+		name: "bad_short",
+		in:   "00:01:02:03",
+	}, {
+		want: assert.False,
+		name: "bad_long",
+		in:   "00:01:02:03:04:05:06:07:08:09:0a:0b:0c:0d:0e:0f:10:11:12:13:14",
+	}, {
+		want: assert.False,
+		name: "bad_eui_48",
+		in:   "00:01:02:03:04:!!",
+	}, {
+		want: assert.False,
+		name: "bad_eui_48_dot",
+		in:   "0000.5e00.!!!!",
+	}, {
+		want: assert.False,
+		name: "bad_eui_64",
+		in:   "00:01:02:03:04:05:06:!!",
+	}, {
+		want: assert.False,
+		name: "bad_infiniband",
+		in:   "00:01:02:03:04:05:06:07:08:09:0a:0b:0c:0d:0e:0f:10:11:12:!!",
+	}}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			tc.want(t, netutil.IsValidMACString(tc.in))
+		})
+	}
+}
+
+func BenchmarkIsValidMACString(b *testing.B) {
+	benchCases := []struct {
+		want require.BoolAssertionFunc
+		name string
+		in   string
+	}{{
+		want: require.True,
+		name: "good_eui_48",
+		in:   "00:01:02:03:04:05",
+	}, {
+		want: require.True,
+		name: "good_eui_48_dot",
+		in:   "0000.5e00.5301",
+	}, {
+		want: require.True,
+		name: "good_eui_48_hyphen",
+		in:   "00-00-5e-00-53-01",
+	}, {
+		want: require.True,
+		name: "good_eui_64",
+		in:   "00:01:02:03:04:05:06:07",
+	}, {
+		want: require.True,
+		name: "good_infiniband",
+		in:   "00:01:02:03:04:05:06:07:08:09:0a:0b:0c:0d:0e:0f:10:11:12:13",
+	}, {
+		want: require.False,
+		name: "bad_empty",
+		in:   "",
+	}, {
+		want: require.False,
+		name: "bad_short",
+		in:   "00:01:02:03",
+	}, {
+		want: require.False,
+		name: "bad_long",
+		in:   "00:01:02:03:04:05:06:07:08:09:0a:0b:0c:0d:0e:0f:10:11:12:13:14",
+	}, {
+		want: require.False,
+		name: "bad_eui_48",
+		in:   "00:01:02:03:04:!!",
+	}, {
+		want: require.False,
+		name: "bad_eui_64",
+		in:   "00:01:02:03:04:05:06:!!",
+	}, {
+		want: require.False,
+		name: "bad_infiniband",
+		in:   "00:01:02:03:04:05:06:07:08:09:0a:0b:0c:0d:0e:0f:10:11:12:!!",
+	}}
+
+	for _, bc := range benchCases {
+		b.Run(bc.name, func(b *testing.B) {
+			var got bool
+			b.ReportAllocs()
+			for b.Loop() {
+				got = netutil.IsValidMACString(bc.in)
+			}
+
+			bc.want(b, got)
+		})
+	}
+
+	// Most recent results:
+	//	goos: darwin
+	//	goarch: arm64
+	//	pkg: github.com/AdguardTeam/golibs/netutil
+	//	cpu: Apple M1 Pro
+	//	BenchmarkIsValidMACString/good_eui_48-8         	64798680	        18.55 ns/op	       0 B/op	       0 allocs/op
+	//	BenchmarkIsValidMACString/good_eui_48_dot-8     	67330148	        18.06 ns/op	       0 B/op	       0 allocs/op
+	//	BenchmarkIsValidMACString/good_eui_48_hyphen-8  	65400295	        18.23 ns/op	       0 B/op	       0 allocs/op
+	//	BenchmarkIsValidMACString/good_eui_64-8         	52725235	        22.59 ns/op	       0 B/op	       0 allocs/op
+	//	BenchmarkIsValidMACString/good_infiniband-8     	24440022	        49.04 ns/op	       0 B/op	       0 allocs/op
+	//	BenchmarkIsValidMACString/bad_empty-8           	587863916	         2.044 ns/op       0 B/op	       0 allocs/op
+	//	BenchmarkIsValidMACString/bad_short-8           	583646232	         2.041 ns/op       0 B/op	       0 allocs/op
+	//	BenchmarkIsValidMACString/bad_long-8            	494821214	         2.413 ns/op       0 B/op	       0 allocs/op
+	//	BenchmarkIsValidMACString/bad_eui_48-8          	67780974	        17.69 ns/op	       0 B/op	       0 allocs/op
+	//	BenchmarkIsValidMACString/bad_eui_64-8          	54463244	        22.02 ns/op	       0 B/op	       0 allocs/op
+	//	BenchmarkIsValidMACString/bad_infiniband-8      	24736444	        48.30 ns/op	       0 B/op	       0 allocs/op
+}
+
+func FuzzIsValidMACString(f *testing.F) {
+	for _, seed := range []string{
+		"",
+		" ",
+		"00",
+		"00:00",
+		"00.00",
+		"00-00",
+		"00:db::68",
+		"!!:00:00:00:00:00",
+	} {
+		f.Add(seed)
+	}
+
+	f.Fuzz(func(t *testing.T, input string) {
+		ok := netutil.IsValidMACString(input)
+		_, err := net.ParseMAC(input)
+
+		require.Equalf(t, err == nil, ok, "input: %q, error: %v", input, err)
+	})
+}
+
 func TestJoinHostPort(t *testing.T) {
 	t.Parallel()
 
